79181c26944371c3a89b3be8e07cd0f06309911e
B10517044 <B10517044@gemail.yuntech.edu.tw>
2020-10-29 15:25:59 +0800

    first commit

--- NSRange/ViewController.m
+++ NSRange/ViewController.m
@@ -8,6 +8,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *mlabel;
 
 @end
 
@@ -15,7 +16,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    //找字串中某個字的位置和長度
+    NSString *str = @"I Love Xcode";
+    NSString *c = @"X";
+    NSRange range = [str rangeOfString:c];
+    NSLog(@"%@", NSStringFromRange(range));
+    
+    //從字串中抓取某個範圍的字
+    NSString *capStr = [[NSString alloc]init];
+    NSRange range1 = {2, 4};
+    NSRange range2 = NSMakeRange(0, 11);
+    capStr = [str substringWithRange:range1];
+    NSLog(@"%@", capStr);
+
+    
 }
 
 
