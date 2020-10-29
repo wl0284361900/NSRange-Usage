8ec52b835d9e2bec13b4a9a2a459621af67171d5
B10517044 <B10517044@gemail.yuntech.edu.tw>
2020-10-29 14:56:50 +0800

    Initial Commit

---
+++ NSRange/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSRange
+//
+//  Created by winproluhao on 2020/10/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
